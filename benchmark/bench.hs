07815576a7ac4876bde5ddfdbb495f88beea5d08
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2011-12-17 02:23:54 +0400

    Add benchmark

---
+++ benchmark/bench.hs
@@ -0,0 +1,22 @@
+import Criterion.Main
+import Numeric.SpecFunctions
+import Text.Printf
+
+main :: IO ()
+main = defaultMain 
+  [ bgroup "logGamma" 
+    [ bench (printf "%.3f" x) $ nf logGamma x
+    | x <- [ 0.1,0.4,0.8                  -- [0     .. 1.5  ]
+           , 2, 3.5                       -- [1.5   .. 4    ]
+           , 5, 7, 11                     -- [4     .. 12   ]
+           , 100, 300, 1e3, 3e3, 1e4, 3e4 -- [12    .. 5.1e4]
+           , 6e4                          -- [5.1e4 .. +∞   ]
+           ]
+    ]
+  , bgroup "logGammaL"
+    [ bench (printf "%.3f" x) $ nf logGammaL x
+    | x <- [ 1e-1, 3e-1, 1e+0, 3e+0
+           , 1e+1, 3e+1, 1e+2, 3e+2, 
+           , 1e+3, 3e+3, 1e+4, 3e+4 
+           ]
+  ]
