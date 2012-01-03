a08bbe8400eb4340fc11903c3e2edf889e4e7e4b
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2012-01-03 21:46:11 +0400

    Add bd0 function from S.Math
    It moved to N.SpecFunctions.Extra module since I think it's rarely
    used and it would just pollute namespace in N.SpecFunctions

---
+++ Numeric/SpecFunctions/Extra.hs
@@ -0,0 +1,33 @@
+-- |
+-- Module    : Numeric.SpecFunctions.Extra
+-- Copyright : (c) 2009, 2011 Bryan O'Sullivan
+-- License   : BSD3
+--
+-- Maintainer  : bos@serpentine.com
+-- Stability   : experimental
+-- Portability : portable
+--
+-- Less common mathematical functions.
+module Numeric.SpecFunctions.Extra (
+  bd0
+  ) where
+
+import Numeric.MathFunctions.Constants (m_NaN)
+
+-- | Evaluate the deviance term @x log(x/np) + np - x@.
+bd0 :: Double                   -- ^ @x@
+    -> Double                   -- ^ @np@
+    -> Double 
+bd0 x np 
+  | isInfinite x || isInfinite np || np == 0 = m_NaN
+  | abs x_np >= 0.1*(x+np)                   = x * log (x/np) - x_np
+  | otherwise                                = loop 1 (ej0*vv) s0
+  where 
+    x_np = x - np
+    v    = x_np / (x+np)
+    s0   = x_np * v
+    ej0  = 2*x*v
+    vv   = v*v
+    loop j ej s = case s + ej/(2*j+1) of
+                    s' | s' == s   -> s'  -- FIXME: Comparing Doubles for equality!
+                       | otherwise -> loop (j+1) (ej*vv) s'
