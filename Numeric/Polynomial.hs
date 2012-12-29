fee7bd3abe254d3c97c37b5f10e2584b5a04bc17
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2012-12-29 19:11:58 +0400

    Add function to evaluate even and odd polynomials

--- Numeric/Polynomial.hs
+++ Numeric/Polynomial.hs
@@ -11,6 +11,8 @@
 -- Chebyshev polynomials.
 module Numeric.Polynomial (
     evaluatePolynomial
+  , evaluateEvenPolynomial
+  , evaluateOddPolynomial
   ) where
 
 import qualified Data.Vector.Generic as G
@@ -25,3 +27,24 @@ evaluatePolynomial :: (Vector v Double)
 {-# INLINE evaluatePolynomial #-}
 evaluatePolynomial x coefs
   = G.foldr (\a r -> a + r*x) 0 coefs
+
+-- | Evaluate polynomial with event powers only using Horner's method
+evaluateEvenPolynomial :: (Vector v Double)
+                       => Double    -- ^ /x/
+                       -> v Double  -- ^ Coefficients
+                       -> Double
+{-# INLINE evaluateEvenPolynomial #-}
+evaluateEvenPolynomial x coefs
+  = G.foldr (\a r -> a + r*x2) 0 coefs
+  where x2 = x * x
+
+-- | Evaluate polynomial with only powers only using Horner's method
+evaluateOddPolynomial :: (Vector v Double)
+                       => Double    -- ^ /x/
+                       -> v Double  -- ^ Coefficients
+                       -> Double
+{-# INLINE evaluateOddPolynomial #-}
+evaluateOddPolynomial x coefs
+  = x * G.foldr (\a r -> a + r*x2) 0 coefs
+  where x2 = x * x
+
