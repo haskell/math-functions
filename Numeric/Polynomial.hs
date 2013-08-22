a57664f53a6e3fe0c95a61de11c94055900c0ca7
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2013-08-22 15:29:13 +0400

    Eta contract

--- Numeric/Polynomial.hs
+++ Numeric/Polynomial.hs
@@ -34,8 +34,8 @@ evaluatePolynomial :: (Vector v a, Num a)
                    -> v a  -- ^ Coefficients
                    -> a
 {-# INLINE evaluatePolynomial #-}
-evaluatePolynomial x coefs
-  = G.foldr (\a r -> a + r*x) 0 coefs
+evaluatePolynomial x
+  = G.foldr (\a r -> a + r*x) 0
 
 -- | Evaluate polynomial with only even powers using Horner's method.
 -- Coefficients starts from lowest. In pseudocode:
@@ -46,8 +46,8 @@ evaluateEvenPolynomial :: (Vector v a, Num a)
                        -> v a  -- ^ Coefficients
                        -> a
 {-# INLINE evaluateEvenPolynomial #-}
-evaluateEvenPolynomial x coefs
-  = G.foldr (\a r -> a + r*x2) 0 coefs
+evaluateEvenPolynomial x
+  = G.foldr (\a r -> a + r*x2) 0
   where x2 = x * x
 
 -- | Evaluate polynomial with only odd powers using Horner's method.
