6974f110342aa70822f4a16211eadd997ef60b0a
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2016-04-26 23:45:58 +0300

    Add tests for comparison

---
+++ tests/Tests/Comparison.hs
@@ -0,0 +1,24 @@
+-- |
+-- Tests for approximate comparison
+module Tests.Comparison (tests) where
+
+import Test.QuickCheck  hiding (choose)
+import Test.Framework
+import Test.Framework.Providers.QuickCheck2
+
+import Tests.Helpers
+import Tests.SpecFunctions.Tables
+
+import Numeric.MathFunctions.Comparison
+import Numeric.MathFunctions.Constants (m_epsilon)
+
+tests :: Test
+tests = testGroup "Comparison"
+  [ testProperty "addUlps 0"       $ \x   -> x == addUlps 0 x
+  , testProperty "addUlps sym"     $ \i x -> x == (addUlps (-i) . addUlps i) x
+  , testProperty "ulpDistance==0"  $ \x   -> ulpDistance x x == 0
+  , testProperty "ulpDistance sym" $ \x y -> ulpDistance x y == ulpDistance y x
+    -- Test that code is correct for m_epsilon
+  , testAssertion "eps distance" $ ulpDistance 1 (1+m_epsilon) == 1
+  , testAssertion "eps add"      $ addUlps 1 1 == 1 + m_epsilon
+  ]
--- tests/tests.hs
+++ tests/tests.hs
@@ -2,9 +2,11 @@ import Test.Framework       (defaultMain)
 import qualified Tests.SpecFunctions
 import qualified Tests.Chebyshev
 import qualified Tests.Sum
+import qualified Tests.Comparison
 
 main :: IO ()
 main = defaultMain [ Tests.SpecFunctions.tests
                    , Tests.Chebyshev.tests
                    , Tests.Sum.tests
+                   , Tests.Comparison.tests
                    ]
