2728d3c465a4ffca88417f3305f398484ccf21a7
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2016-04-26 23:19:07 +0300

    Add function to manipulate Double in term of ulps and function to calculate distance in ulps

--- Numeric/MathFunctions/Comparison.hs
+++ Numeric/MathFunctions/Comparison.hs
@@ -12,12 +12,57 @@
 -- <http://www.cygnus-software.com/papers/comparingfloats/comparingfloats.htm>
 module Numeric.MathFunctions.Comparison
     (
-      within
+      addUlps
+    , ulpDistance
+    , within
     ) where
 
 import Control.Monad.ST (runST)
 import Data.Primitive.ByteArray (newByteArray, readByteArray, writeByteArray)
 import Data.Word (Word64)
+import Data.Int (Int64)
+
+
+
+-- |
+-- Add N ULPs (units of least precision) to @Double@ number.
+addUlps :: Int -> Double -> Double
+addUlps n a = runST $ do
+  buf <- newByteArray 8
+  ai0 <- writeByteArray buf 0 a >> readByteArray buf 0
+  -- Convert to ulps number represented as Int64
+  let big     = 0x8000000000000000
+      order :: Word64 -> Int64
+      order i | i < big   = fromIntegral i
+              | otherwise = fromIntegral $ maxBound - (i - big)
+      unorder :: Int64 -> Word64
+      unorder i | i >= 0    = fromIntegral i
+                | otherwise = big + (maxBound - (fromIntegral i))
+  let ai0' = unorder $ order ai0 + fromIntegral n
+  writeByteArray buf 0 ai0' >> readByteArray buf 0
+
+-- |
+-- Measure distance between two @Double@s in ULPs (units of least
+-- precision).
+ulpDistance :: Double
+            -> Double
+            -> Word64
+ulpDistance a b = runST $ do
+  buf <- newByteArray 8
+  ai0 <- writeByteArray buf 0 a >> readByteArray buf 0
+  bi0 <- writeByteArray buf 0 b >> readByteArray buf 0
+  -- IEEE754 floats use most significant bit as sign bit (not
+  -- 2-complement) and we need to rearrange representations of float
+  -- number so that they could be compared lexicographically as
+  -- Word64.
+  let big     = 0x8000000000000000
+      order i | i < big   = i + big
+              | otherwise = maxBound - i
+      ai = order ai0
+      bi = order bi0
+      d  | ai > bi   = ai - bi
+         | otherwise = bi - ai
+  return $! d
 
 -- | Compare two 'Double' values for approximate equality, using
 -- Dawson's method.
@@ -27,13 +72,6 @@ import Data.Word (Word64)
 -- or less, this function returns @True@.
 within :: Int                   -- ^ Number of ULPs of accuracy desired.
        -> Double -> Double -> Bool
-within ulps a b = runST $ do
-  buf <- newByteArray 8
-  ai0 <- writeByteArray buf 0 a >> readByteArray buf 0
-  bi0 <- writeByteArray buf 0 b >> readByteArray buf 0
-  let big  = 0x8000000000000000 :: Word64
-      ai | ai0 < 0   = big - ai0
-         | otherwise = ai0
-      bi | bi0 < 0   = big - bi0
-         | otherwise = bi0
-  return $ abs (ai - bi) <= fromIntegral ulps
+within ulps a b
+  | ulps < 0  = False
+  | otherwise = ulpDistance a b <= fromIntegral ulps
