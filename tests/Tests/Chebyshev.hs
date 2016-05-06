f9da7728958606d8442c769c57da3f403b3ccddb
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2016-05-06 18:43:55 +0300

    Reenable failing tests for Chebyshev polynomials

--- tests/Tests/Chebyshev.hs
+++ tests/Tests/Chebyshev.hs
@@ -11,41 +11,49 @@ import Test.QuickCheck                      (Arbitrary(..),counterexample,Proper
 
 import Tests.Helpers
 import Numeric.Polynomial.Chebyshev
+import Numeric.MathFunctions.Comparison
 
 
 tests :: Test
 tests = testGroup "Chebyshev polynomials"
   [ testProperty "Chebyshev 0" $ \a0 (Ch x) ->
       testCheb [a0] x
-  -- XXX FIXME DISABLED due to failure
-  -- , testProperty "Chebyshev 1" $ \a0 a1 (Ch x) ->
-  --   testCheb [a0,a1] x
-  -- , testProperty "Chebyshev 2" $ \a0 a1 a2 (Ch x) ->
-  --   testCheb [a0,a1,a2] x
-  -- , testProperty "Chebyshev 3" $ \a0 a1 a2 a3 (Ch x) ->
-  --   testCheb [a0,a1,a2,a3] x
-  -- , testProperty "Chebyshev 4" $ \a0 a1 a2 a3 a4 (Ch x) ->
-  --   testCheb [a0,a1,a2,a3,a4] x
-  -- , testProperty "Broucke" $ testBroucke
+  , testProperty "Chebyshev 1" $ \a0 a1 (Ch x) ->
+      testCheb [a0,a1] x
+  , testProperty "Chebyshev 2" $ \a0 a1 a2 (Ch x) ->
+      testCheb [a0,a1,a2] x
+  , testProperty "Chebyshev 3" $ \a0 a1 a2 a3 (Ch x) ->
+      testCheb [a0,a1,a2,a3] x
+  , testProperty "Chebyshev 4" $ \a0 a1 a2 a3 a4 (Ch x) ->
+      testCheb [a0,a1,a2,a3,a4] x
+  , testProperty "Broucke" $ testBroucke
   ]
   where
 
-testBroucke :: Ch -> [Double] -> Bool
-testBroucke _      []     = True
-testBroucke (Ch x) (c:cs) = let c1 = chebyshev        x (fromList $ c : cs)
-                                cb = chebyshevBroucke x (fromList $ c*2 : cs)
-                            in eq 1e-15 c1 cb
+testBroucke :: Ch -> [Double] -> Property
+testBroucke _      []     = counterexample "" True
+testBroucke (Ch x) (c:cs)
+  = counterexample (">>> Chebyshev  = " ++ show c1)
+  $ counterexample (">>> Brouke     = " ++ show cb)
+  $ counterexample (">>> rel.err.   = " ++ show (relativeError c1 cb))
+  $ counterexample (">>> diff. ulps = " ++ show (ulpDistance   c1 cb))
+  $ within 64 c1 cb
+  where
+    c1 = chebyshev        x (fromList $ c : cs)
+    cb = chebyshevBroucke x (fromList $ c*2 : cs)
+
 
 testCheb :: [Double] -> Double -> Property
 testCheb as x
-  = counterexample (">>> Exact   = " ++ show exact)
-  $ counterexample (">>> Numeric = " ++ show num  )
-  $ counterexample (">>> rel.err.= " ++ show err  )
+  = counterexample (">>> Exact      = " ++ show exact)
+  $ counterexample (">>> Numeric    = " ++ show num  )
+  $ counterexample (">>> rel.err.   = " ++ show err  )
+  $ counterexample (">>> diff. ulps = " ++ show (ulpDistance num exact))
   $ eq 1e-12 num exact
   where
     exact = evalCheb as x
     num   = chebyshev x (fromList as)
-    err   = abs (num - exact) / abs exact
+    err   = relativeError num exact
 
 evalCheb :: [Double] -> Double -> Double
 evalCheb as x
