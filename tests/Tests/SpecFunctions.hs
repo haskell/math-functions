a8291ee61a0e5986c67ad36de50aa813433419d8
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2019-09-07 23:56:21 +0300

    Group everything together

--- tests/Tests/SpecFunctions.hs
+++ tests/Tests/SpecFunctions.hs
@@ -52,48 +52,35 @@ tests = testGroup "Special functions"
   ----------------
   , testGroup "incomplete gamma"
     [ testProperty "incomplete gamma - increases" $
-      \(abs -> s) (abs -> x) (abs -> y) -> s > 0 ==> monotonicallyIncreases (incompleteGamma s) x y
+        \(abs -> s) (abs -> x) (abs -> y) -> s > 0 ==> monotonicallyIncreases (incompleteGamma s) x y
     , testProperty "0 <= gamma <= 1"               incompleteGammaInRange
     , testProperty "gamma(1,x) = 1 - exp(-x)"      incompleteGammaAt1Check
     , testProperty "invIncompleteGamma = gamma^-1" invIGammaIsInverse
     ]
   ----------------
-  , testProperty "0 <= I[B] <= 1"                incompleteBetaInRange
-  -- XXX FIXME DISABLED due to failures
-  -- , testProperty "invIncompleteBeta  = B^-1" $ invIBetaIsInverse
-    -- Unit tests
-  , testAssertion "Factorial is expected to be precise at 1e-15 level"
-      $ and [ eq 1e-15 (factorial (fromIntegral n :: Int))
-                       (fromIntegral (factorial' n))
-            |n <- [0..170]]
-  , testAssertion "Log factorial is expected to be precise at 1e-15 level"
-      $ and [ eq 1e-15 (logFactorial (fromIntegral n :: Int))
-                       (log $ fromIntegral $ factorial' n)
-            | n <- [2..170]]
-    -- FIXME: loss of precision when logBeta p q ≈ 0.
-    --        Relative error doesn't work properly in this case.
-  , testAssertion "logBeta is expected to be precise at 1e-6 level"
+  , testGroup "beta function"
+    [ testAssertion "logBeta is expected to be precise at 1e-6 level"
       $ and [ eq 1e-6 (logBeta p q)
                       (logGammaL p + logGammaL q - logGammaL (p+q))
             | p <- [0.1,0.2 .. 0.9] ++ [2 .. 20]
             , q <- [0.1,0.2 .. 0.9] ++ [2 .. 20]
             ]
-  , testAssertion "digamma is expected to be precise at 1e-14 [integers]"
-      $ digammaTestIntegers 1e-14
-    -- Relative precision is lost when digamma(x) ≈ 0
-  , testAssertion "digamma is expected to be precise at 1e-12"
-      $ and [ eq 1e-12 r (digamma x) | (x,r) <- tableDigamma ]
-    --
-  , let deviations = [ ( "p=",p, "q=",q, "x=",x
-                       , "ib=",ib, "ib'=",ib'
-                       , "err=",relativeError ib ib' / m_epsilon)
-                     | (p,q,x,ib) <- tableIncompleteBeta
-                     , let ib' = incompleteBeta p q x
-                     , not $ eq (64 * m_epsilon) ib' ib
-                     ]
-    in testCase "incompleteBeta is expected to be precise at 32*m_epsilon level"
-     $ assertBool (unlines (map show deviations)) (null deviations)
-  , testAssertion "incompleteBeta with p > 3000 and q > 3000"
+    ]
+  ----------------
+  , testGroup "incomplete beta"
+    [ testProperty "0 <= I[B] <= 1"                incompleteBetaInRange
+    -- XXX FIXME DISABLED due to failures
+    -- , testProperty "invIncompleteBeta  = B^-1" $ invIBetaIsInverse
+    , let deviations = [ ( "p=",p, "q=",q, "x=",x
+                         , "ib=",ib, "ib'=",ib'
+                         , "err=",relativeError ib ib' / m_epsilon)
+                       | (p,q,x,ib) <- tableIncompleteBeta
+                       , let ib' = incompleteBeta p q x
+                               , not $ eq (64 * m_epsilon) ib' ib
+                       ]
+      in testCase "incompleteBeta is expected to be precise at 32*m_epsilon level"
+      $ assertBool (unlines (map show deviations)) (null deviations)
+    , testAssertion "incompleteBeta with p > 3000 and q > 3000"
       $ and [ eq 1e-11 (incompleteBeta p q x) ib | (x,p,q,ib) <-
                  [ (0.495,  3001,  3001, 0.2192546757957825068677527085659175689142653854877723)
                  , (0.501,  3001,  3001, 0.5615652382981522803424365187631195161665429270531389)
@@ -101,17 +88,44 @@ tests = testGroup "Special functions"
                  , (0.501, 13500, 13200, 0.0656209987264794057358373443387716674955276089622780)
                  ]
             ]
-  , testAssertion "choose is expected to precise at 1e-12 level"
+    ]
+  ----------------
+  , testGroup "digamma"
+    [ -- FIXME: loss of precision when logBeta p q ≈ 0.
+      --        Relative error doesn't work properly in this case.
+      testAssertion "digamma is expected to be precise at 1e-14 [integers]"
+        $ digammaTestIntegers 1e-14
+      -- Relative precision is lost when digamma(x) ≈ 0
+    , testAssertion "digamma is expected to be precise at 1e-12"
+        $ and [ eq 1e-12 r (digamma x) | (x,r) <- tableDigamma ]
+    ]
+  ----------------
+  , testGroup "factorial"
+    [ testAssertion "Factorial is expected to be precise at 1e-15 level"
+      $ and [ eq 1e-15 (factorial (fromIntegral n :: Int))
+                       (fromIntegral (factorial' n))
+            | n <- [0..170]]
+    , testAssertion "Log factorial is expected to be precise at 1e-15 level"
+      $ and [ eq 1e-15 (logFactorial (fromIntegral n :: Int))
+                       (log $ fromIntegral $ factorial' n)
+            | n <- [2..170]]
+    ]
+  ----------------
+  , testGroup "combinatorics"
+    [ testAssertion "choose is expected to precise at 1e-12 level"
       $ and [ eq 1e-12 (choose (fromIntegral n) (fromIntegral k)) (fromIntegral $ choose' n k)
             | n <- [0..1000], k <- [0..n]]
-  , testAssertion "logChoose == log . choose"
+    , testAssertion "logChoose == log . choose"
       $ and [ let n' = fromIntegral n
                   k' = fromIntegral k
               in within 2 (logChoose n' k') (log $ choose n' k')
             | n <- [0::Int .. 1000], k <- [0 .. n]]
+    ]
     ----------------------------------------------------------------
     -- Self tests
-  , testProperty "Self-test: 0 <= range01 <= 1" $ \x -> let f = range01 x in f <= 1 && f >= 0
+  , testGroup "self-test"
+    [ testProperty "Self-test: 0 <= range01 <= 1" $ \x -> let f = range01 x in f <= 1 && f >= 0
+    ]
   ]
 
 ----------------------------------------------------------------
